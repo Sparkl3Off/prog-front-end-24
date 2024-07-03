e9c97635950c199021785a786f2a1a566f59ecb4
Sparkl3Off <danielrebel1605@gmail.com>
2024-07-03 09:41:23 -0400

    useState

--- src/components/Inicio.js
+++ src/components/Inicio.js
@@ -2,7 +2,12 @@ import React, { useEffect, useState } from "react";
 
 const Inicio = () => {
     return (
-        <h1>Hola desde Inicio</h1>
+        <>
+            <h1>Hola desde Inicio</h1>
+            <div>
+                <Link to="/Perfil"></Link>
+            </div>
+        </>
     );
 }
 export default Inicio;
--- src/components/Perfil.js
+++ src/components/Perfil.js
@@ -1,8 +1,18 @@
 import React, { useEffect, useState } from "react";
 
 const Perfil = () => {
+    // Contador de "Likes"
+    const [contadorLikes, setContadorLikes] = useState(0);
+    const handleContador = () => {
+        setContadorLikes(contadorLikes+1);
+        
+    };
     return (
-        <h1>Estoy en Perfil</h1>
+        <>
+            <h1>Estoy en Perfil</h1>
+            <button type="button" onClick={handleContador}>Sumar Like</button>
+            <p>Conteo de likes: {contadorLikes}</p>
+        </>
     );
 }
 export default Perfil;
